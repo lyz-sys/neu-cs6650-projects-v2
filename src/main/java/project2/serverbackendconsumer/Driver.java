be96690da041207097d4fffe59f18c46bfff30d9
Yizhou Li <lyzpp2000@163.com>
2024-02-27 20:24:57 -0800

    code refactor

--- src/main/java/project2/serverbackendconsumer/Driver.java
+++ src/main/java/project2/serverbackendconsumer/Driver.java
@@ -9,35 +9,22 @@ import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeoutException;
 import lombok.extern.slf4j.Slf4j;
-
+import project2.server.RabbitMQUtil;
 import project2.tools.Configuration;
 
 @Slf4j
 public class Driver {
-    private static final Configuration CONFIG = new Configuration(); // todo: try rmq util?
-    private static final String QUEUE_NAME = CONFIG.getRmqMainQueueName();
     private static final ConcurrentHashMap<String, List<String>> liftRidesMap = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(CONFIG.getRmqHostAddress());
-        factory.setUsername(CONFIG.getRmqUsername());
-        factory.setPassword(CONFIG.getRmqPassword());
         for (int i = 0; i < CONFIG.getConsumerThreadNum(); i++) {
             new Thread(() -> {
                 try {
-                    final Connection connection = factory.newConnection();
-                    final Channel channel = connection.createChannel();
-                    channel.queueDeclare(QUEUE_NAME, false, false, false, null); // tip: the cnannel parameters must be
-                                                                                 // same for both producer and consumer
-
                     DeliverCallback deliverCallback = (consumerTag, delivery) -> {
                         String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
                         processMessage(message);
                     };
-
-                    channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
-                    });
+                    RabbitMQUtil.consumeMessage(deliverCallback);
                 } catch (IOException | TimeoutException e) {
                     e.printStackTrace();
                 }
