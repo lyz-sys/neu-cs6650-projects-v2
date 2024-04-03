e130a4f55c5b616f3333e4525f05070351467bef
Yizhou Li <lyzpp2000@163.com>
2024-04-02 17:33:32 -0700

    p4 batchwrite

--- src/main/java/project4/server/db/DynamoDBController.java
+++ src/main/java/project4/server/db/DynamoDBController.java
@@ -5,10 +5,14 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.WriteRequest;
+import software.amazon.awssdk.services.dynamodb.model.BatchWriteItemRequest;
 import software.amazon.awssdk.services.dynamodb.model.QueryRequest;
 import software.amazon.awssdk.services.dynamodb.model.QueryResponse;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.services.dynamodb.model.PutRequest;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
@@ -20,11 +24,9 @@ import lombok.extern.slf4j.Slf4j;
 public class DynamoDBController {
     // Create the session credentials object
     private AwsSessionCredentials awsSessionCredentials = AwsSessionCredentials.create(
-            "ASIAX3JQJHMPZHCHDZ22", // Your access key ID
-            "MaQAZUxACRiSgha6yr7mui/DSwVdfO8dZU8EUeUC", // Your secret access key
-            "FwoGZXIvYXdzEL3//////////wEaDA9YU0tFUNh4oJdTUyLLATunUYGDkg6VnPchl8zl3hTlqIqtFHns8ZujwEYWYcVDSbZGO/ErFTths4dRtjzlbflfUr5GULAz0pTAqMe5hSeX3PWDFMOh6P0mk7O0u9pCrgWkWo93NuS2NcDNw2KzaLf8MXX4TehE3/QBnVFQi/9TcLXebWv9N3Nu372j/Sfw81CjRZPzkpZSI/x7VGhpBz0Bz6K0dLcJDfXPg1tSVcJHew0OO+fb6W5o3Ptgl/seOECqoGAnYo8FPMiRNzW2BdJq1i+NZYTjCdJlKKP3kbAGMi0ea83dfB/b2N57nM9AjDKpKOKALAbyCVxo0LQomZWQW3LS2eX/NP0tuVxUczA=" // Your
-                                                                                                                                                                                                                                                                                                                                                                                                                      // session
-                                                                                                                                                                                                                                                                                                                                                                                                                      // token
+            "ASIAX3JQJHMP56CLVG5W", // Your access key ID
+            "7l1IXcSHfSlZmJHbSuMc9qaetaTgqgBSq9aVkj4R", // Your secret access key
+            "IQoJb3JpZ2luX2VjED4aCXVzLXdlc3QtMiJHMEUCIQCvyZgmzpLj0RWBuF29ZIKyjhffIWjHIpb2V58MhjT7pwIgPIaVnWQktO4VKmsR/l7kpKZlhtxqSPzj1V3PR4z2dCoqtAIIZxAAGgw1Mzk2NTY1MzQ4MTUiDG2ZJHUFzERdFjrIfCqRArkDbkxqI8pl7tBsRQFPvFRwueOhBXYvKvUp2oMVKEi4uxNnwd2K2xtiqvAJcalvvyfnlxoaXXLsYyXfVAWDLwZ47a0LHXPhJhOb5V40VtTEZKlNEWEI8nmX7Xd/V1JJA7zGyLPeTdOL7FGHoG7mTeLxbQZa/KTaS7KSJOF6GeSe/A9qvT74TNsizcu7OXg9IYXHa4KvB7s4OoPzFsiPhkwSxP6MZzZ810foELoykHYeBBbQgifLfppjfumA+5BuCQ3g44sSowSWkNRvfSF9HwRKRF+KjLoaivfXEReGD6zHRTQXw6GcKXwNZ3t94igv3JWdHQ/RLvu1zA0SuzDYWe50e8m2QltMFSyD6Fe2f8wkDTC4grKwBjqdAbl5o2rvpt/IJV75OOxCZwHHv8ni9DvdE9i4w3LBEAtf9G+K9zHT46pKUMwkZFtDZSfEuTwMQt/bIeNIqq3i59Sel9KDYh3rGrxHXnvIyyd8CBMgFfee5j6udSzBobxF0YsiLvFVgnSKtI7kEgDJ2EeRmAFb8Zzxfyn5N7ER77jJJiE4Oha4Vtoaqie/6C6OVNXN5pSAsGg/+Apy4aI=" // Your session token
     );
 
     private DynamoDbClient dynamoDbClient = DynamoDbClient.builder()
@@ -56,7 +58,9 @@ public class DynamoDBController {
 
     public void updateSkIdTable(ConcurrentMap<String, List<String>> liftRidesMap) {
         int itemCount = 0;
-        while (itemCount < 200000) { 
+        final int BATCH_SIZE = 25;
+        List<WriteRequest> batch = new ArrayList<>(BATCH_SIZE);
+        while (itemCount < 200000) {
             for (Iterator<Map.Entry<String, List<String>>> it = liftRidesMap.entrySet().iterator(); it.hasNext();) {
                 Map.Entry<String, List<String>> entry = it.next();
                 String skierId = entry.getKey();
@@ -73,30 +77,41 @@ public class DynamoDBController {
                 String dayID = rides.get(2);
                 String liftID = rides.get(3);
                 String time = rides.get(4);
+                String sortKey = String.join("#", resortID, seasonID, dayID, time);
 
-                try {
-                    String sortKey = String.join("#", resortID, seasonID, dayID, time);
+                Map<String, AttributeValue> itemAttributes = Map.of(
+                        "primKey", AttributeValue.builder().s(skierId).build(),
+                        "sortKey", AttributeValue.builder().s(sortKey).build(),
+                        "liftID", AttributeValue.builder().s(liftID).build(),
+                        "verticals", AttributeValue.builder().n(String.valueOf(10 * Integer.parseInt(liftID))).build());
 
-                    PutItemRequest putItemRequest = PutItemRequest.builder()
-                            .tableName("skInfo")
-                            .item(Map.of(
-                                    "primKey", AttributeValue.builder().s(skierId).build(),
-                                    "sortKey", AttributeValue.builder().s(sortKey).build(),
-                                    "liftID", AttributeValue.builder().s(liftID).build(),
-                                    "verticals", AttributeValue.builder().n(String.valueOf(10 * Integer.parseInt(liftID))).build()))
-                            .build();
+                PutRequest putRequest = PutRequest.builder().item(itemAttributes).build();
+                WriteRequest writeRequest = WriteRequest.builder().putRequest(putRequest).build();
 
-                    dynamoDbClient.putItem(putItemRequest);
-                    itemCount++;
-                } catch (Exception e) {
-                    log.error("Error putting item to DynamoDB: " + e.getMessage(), e);
-                } finally {
-                    it.remove();
+                synchronized (batch) {
+                    batch.add(writeRequest);
+                    if (batch.size() == BATCH_SIZE) {
+                        submitBatch(batch);
+                        batch.clear(); // Prepare for next batch
+                    }
                 }
 
+                itemCount++;
+                it.remove();
                 log.info("item count: " + itemCount);
             }
         }
+        if (!batch.isEmpty()) {
+            submitBatch(batch);
+        }
         log.info("Finished updating skInfo. Total items processed: " + itemCount);
+    }
+
+    private void submitBatch(List<WriteRequest> batch) {
+        BatchWriteItemRequest batchWriteItemRequest = BatchWriteItemRequest.builder()
+                .requestItems(Map.of("skInfo", batch))
+                .build();
+
+        dynamoDbClient.batchWriteItem(batchWriteItemRequest);
     }
 }
